62fa6d5d4610dcfe7fe72c0771e1943f9c6d71a5
akutafina <anastasia.kutafina@trivago.com>
2023-08-25 15:09:55 +0200

    Fixes the tests names

--- cypress/e2e/smoke/open-all-pages-specs.cy.js
+++ cypress/e2e/smoke/open-all-pages-specs.cy.js
@@ -11,32 +11,32 @@ import {visibilitySteps} from "../../steps/visibility-steps";
 
 describe('Loads all the pages (browser: ' + Cypress.browser.name + ', viewport: ' + Cypress.config('viewportWidth') + 'x' + Cypress.config('viewportHeight') + ')', () => {
 
-    it(`Visibility page loads and has title`, () => {
+    it(`Ajax Data page loads and has title`, () => {
         ajaxDataSteps.visit();
         ajaxDataSteps.hasTitle();
     })
 
-    it(`Visibility page loads and has title`, () => {
+    it(`Dynamic Table page loads and has title`, () => {
         dynamicTableSteps.visit();
         dynamicTableSteps.hasTitle();
     })
 
-    it(`Visibility page loads and has title`, () => {
+    it(`Overlapped Elements page loads and has title`, () => {
         overlappedElementSteps.visit();
         overlappedElementSteps.hasTitle();
     })
 
-    it(`Visibility page loads and has title`, () => {
+    it(`Progress bar page loads and has title`, () => {
         progressBarSteps.visit();
         progressBarSteps.hasTitle();
     })
 
-    it(`Visibility page loads and has title`, () => {
+    it(`Sample App page loads and has title`, () => {
         sampleAppSteps.visit();
         sampleAppSteps.hasTitle();
     })
 
-    it(`Visibility page loads and has title`, () => {
+    it(`Text Input page loads and has title`, () => {
         textInputSteps.visit();
         textInputSteps.hasTitle();
     })
@@ -45,48 +45,5 @@ describe('Loads all the pages (browser: ' + Cypress.browser.name + ', viewport: 
         visibilitySteps.visit();
         visibilitySteps.hasTitle();
     })
-
-    // beforeEach(() => {
-    //     visibilitySteps.visit();
-    // })
-    //
-    // it(`verifies the header`, () => {
-    //     visibilitySteps.titleTextIs('Visibility');
-    // })
-    //
-    // it('removes "Removed" button after clicking on "Hide" button', () => {
-    //     visibilitySteps.clickHideBtn()
-    //     // visibilitySteps.removedBtnIsNotInDom()
-    // })
-    //
-    // it('perceived "Zero Width" button as invisible after clicking on "Hide" button', () => {
-    //     visibilitySteps.clickHideBtn()
-    //     visibilitySteps.zeroWidthBtnIsInvisible()
-    // })
-    //
-    // it('overlaps "Overlapped" button when clicking on "Hide" button', () => {
-    //     visibilitySteps.clickHideBtn()
-    //     visibilitySteps.overlappedBtnIsOverlapped()
-    // })
-    //
-    // it('perceived "Opacity 0" button as invisible after clicking on "Hide" button', () => {
-    //     visibilitySteps.clickHideBtn()
-    //     visibilitySteps.opacityZeroBtnIsInvisible()
-    // })
-    //
-    // it('perceived "Visibility Hidden" button as invisible after clicking on "Hide" button', () => {
-    //     visibilitySteps.clickHideBtn()
-    //     visibilitySteps.visibilityHiddenBtnIsInvisible()
-    // })
-    //
-    // it('perceived "Display None" button as invisible after clicking on "Hide" button', () => {
-    //     visibilitySteps.clickHideBtn()
-    //     visibilitySteps.displayNoneBtnIsInvisible()
-    // })
-    //
-    // it('moves "Offscreen" button outside viewport after clicking on "Hide" button', () => {
-    //     visibilitySteps.clickHideBtn()
-    //     visibilitySteps.offscreenBtnIsOutsideViewport()
-    // })
 
 })
